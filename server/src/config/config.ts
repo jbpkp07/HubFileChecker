6a351f91cecc21071e6575774983641bd38530bd
jbpkp07 <jeremy.w.barnes@gmail.com>
2020-04-01 05:22:28 -0700

    fixing static assets path and html path

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -19,6 +19,6 @@ export const config: IConfig = {
     MONGODB_URI: (process.env.MONGODB_URI !== undefined) ? process.env.MONGODB_URI : "mongodb://hub-graviton:S9GtvpEuCEKsotRiUeQLXkWGaDRq52Y9Hi0bvXIi1iJHB0v8wd4LXhKOquqjJNK7nixp35dEMYSKSw4XqDhFAA%3D%3D@hub-graviton.mongo.cosmos.azure.com:10255/hub-graviton?ssl=true",
     port: (process.env.PORT !== undefined) ? process.env.PORT : "3001",
 
-    htmlAssetPath: getFullPath("../../../renderer/build/index.html"),
-    staticAssetsPath: getFullPath("../../../renderer/build")
+    htmlAssetPath: getFullPath("../../../client/build/index.html"),
+    staticAssetsPath: getFullPath("../../../client/build")
 };
